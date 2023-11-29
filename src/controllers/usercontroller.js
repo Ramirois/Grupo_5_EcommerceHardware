259dc9b0f7173aeae0d7e8fd2f99324960fbfaad
AngelmAsaf <angel_asaf@hotmail.com>
2023-11-29 16:52:36 -0300

    rutas y controladores listos

---
+++ src/controllers/usercontroller.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const userController = {
+    login: (req,res)=>{
+    res.sendFile(path.join(__dirname, '../views/login.html'));
+},
+
+    register: (req,res)=>{
+        res.sendFile(path.resolve(__dirname, '../views/register.html'));
+}
+}
+
+module.exports = userController;
