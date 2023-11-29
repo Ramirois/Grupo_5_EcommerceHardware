df1ac3c34af8dd9cc797507b04d0eef8cf6ed799
gustavocortez <crt.gustavo@gmail.com>
2023-11-29 14:55:03 -0300

    controlador + rutas

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require ('express')
 const path = require ('path');
 const app = express();
+const rutas = require('./src/routes/mainRoutes');
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, './src/views'));
 
 app.listen(3000, () =>{
     console.log("Servidor corriendo en el servidor 3000");
 });
 
-app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './src/views/index.html'));
-})
+app.use('/', rutas);
 
 app.get('/login.html', (req,res) => {
     res.sendFile(path.resolve(__dirname, './src/views/login.html'));
@@ -30,7 +32,7 @@ app.get('/login.html', (req, res) => {
 //checar si está bien escrito el nombre de la ruta
 
 app.get('/productCart.html', (req,res)=> {
-    res.sendFile(path.resolve(__dirname, './views/productCart.html'));
+    res.sendFile(path.resolve(__dirname, './src/views/productCart.html'));
 })
 app.get('/register.html', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/register.html'))
---
+++ src/controllers/mainController.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const mainController = {
+index: (req,res) => {
+    res.sendFile(path.resolve(__dirname, '../views/index.html'));
+}
+
+}
+
+module.exports = mainController;
+
