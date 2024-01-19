2cc47ed5fbf21e1646cae49bd6745d8cd7c0ba41
gustavocortez <crt.gustavo@gmail.com>
2024-01-19 16:50:09 -0300

    carpeta database

---
+++ src/database/models/Product.js
@@ -0,0 +1,54 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Product'; // esto debería estar en singular
+    let cols = {
+        id: {
+            type: dataTypes.BIGINT(10).UNSIGNED,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        // created_at: dataTypes.TIMESTAMP,
+        // updated_at: dataTypes.TIMESTAMP,
+        nombre: {
+            type: dataTypes.STRING(50),
+            allowNull: false
+        },
+        descripcion: {
+            type: dataTypes.STRING(200)
+        },
+        awards: {
+            type: dataTypes.BIGINT(10).UNSIGNED,
+            allowNull: false
+        },
+        release_date: {
+            type: dataTypes.DATEONLY,
+            allowNull: false
+        },
+        length: dataTypes.BIGINT(10),
+        genre_id: dataTypes.BIGINT(10)
+    };
+    let config = {
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAt: false
+    }
+    const Movie = sequelize.define(alias,cols,config);
+
+    Movie.associate = function (models) {
+        Movie.belongsTo(models.Genre, { // models.Genre -> Genres es el valor de alias en genres.js
+            as: "genre",
+            foreignKey: "genre_id"
+        })
+
+        Movie.belongsToMany(models.Actor, { // models.Actor -> Actors es el valor de alias en actor.js
+            as: "actors",
+            through: 'actor_movie',
+            foreignKey: 'movie_id',
+            otherKey: 'actor_id',
+            timestamps: false
+        })
+    }
+
+    return Movie
+};
